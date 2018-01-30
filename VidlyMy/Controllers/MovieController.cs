7054dd2f9275f9e2cf27d763c13837e9866a1f35
Corvin Vasa <Corvinz123@gmail.com>
2018-01-30 13:37:05 +0300

    add view

---
+++ VidlyMy/Controllers/MovieController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using asp_les_1.Models;
+using asp_les_1.ViewModels;
+
+namespace asp_les_1.Controllers
+
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "Cust 1 "},
+                new Customer {Name = "Cust 2 "}
+            };
+
+            var viewModel = new RandomMovieViewModel()
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+
+        }
+        /*[Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
+        public ActionResult ByReleaseYear(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+        */
+    }
+}
---
+++ VidlyMy/ViewModels/RandomMovieViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using asp_les_1.Models;
+
+namespace asp_les_1.ViewModels
+{
+    public class RandomMovieViewModel
+    {
+        public Movie Movie { get; set; }
+
+        public List<Customer> Customers { get; set; }
+
+    }
+}
