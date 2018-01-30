7660e936ba48a6947021e76f37cb014bcd951847
Corvin Vasa <Corvinz123@gmail.com>
2018-01-30 14:10:45 +0300

    AddMigrations

--- VidlyMy/Models/Customer.cs
+++ VidlyMy/Models/Customer.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using VidlyMy.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace asp_les_1.Models
 {
     public class Customer
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
-       // public bool IsSubscribedToNewsletter { get; set; }
-        //public MembershipTypeof MembershipTypeof { get; set; }
-       // public byte MembershipTypeofId { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+        public MembershipType MembershipType { get; set; }
+        public byte MembershipTypeId { get; set; }
     }
 }
---
+++ VidlyMy/Models/MembershipType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VidlyMy.Models
+{
+    public class MembershipType
+    {
+        public byte Id { get; set; }
+        public short SignUpFree { get; set; }
+        public byte DurationInMonth { get; set; }
+        public byte DiscountRate { get; set; }
+        
+    }
+}
